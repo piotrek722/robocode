1801f5f0f7795277f34bebe0fd1e17b833a4cb75
Piotr Martyna <piotrek722@gmail.com>
2018-04-29 19:13:05 +0200

    Init commit

---
+++ robo/src/MyRobot.java
@@ -0,0 +1,17 @@
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class MyRobot extends Robot {
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+}
